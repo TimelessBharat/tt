0f6353729acf7cabf20b2d7b2a2e86a7412083e7
TimelessBharat <amartanpure4@gmail.com>
2025-04-15 01:04:45 -0700

    Add files via upload

---
+++ database/connection.php
@@ -0,0 +1,18 @@
+<?php
+    
+    $servername="localhost";
+    $userName="root";
+    $userPassw="";
+    $database="bhartwebdata";
+
+    $coon=new mysqli($servername,$userName,$userPassw,$database);
+    // if ($coon)
+    // {
+    //   echo "Connection sucess fully";
+    // }
+
+    // $insert="INSERT INTO `booking_table` (`Your_Name`, `Your_Email`, `Date_Time`, `Destination`, `Person`, `Kids`, `Special_request`) VALUES ('shirgire Ajit Bhimrav', 'ajitshirgire122@gmail.com', '12-03-2025', 'solapur', '3', '5', 'i am alone') ";
+
+    // mysqli_query($coon,$insert);
+
+?>
